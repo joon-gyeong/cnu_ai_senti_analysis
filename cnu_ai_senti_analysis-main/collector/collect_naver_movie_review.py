18bf1693c721568f6feb898398f4f42c42228f02
joon-gyeong <114456431+joon-gyeong@users.noreply.github.com>
2022-10-20 11:30:37 +0900

    Add files via upload

---
+++ cnu_ai_senti_analysis-main/collector/collect_naver_movie_review.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+#######################
+# 1. 영화 제목 수집
+#######################
+
+# movie_code: 네이버 영화 코드(6자리 숫자)
+
+# 제목 수집
+def movie_title_crawler(url):
+    pass
+
+# 리뷰 수집(리뷰, 평점, 작성자, 작성일자)
+def movie_reviw_crawler(movie_code):
+    pass
