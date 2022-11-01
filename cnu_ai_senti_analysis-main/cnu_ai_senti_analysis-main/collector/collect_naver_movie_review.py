5f2fcf85eb10230aab0f6e803c4b3abdc09bde4a
joon-gyeong <114456431+joon-gyeong@users.noreply.github.com>
2022-11-01 11:31:50 +0900

    Add files via upload

---
+++ cnu_ai_senti_analysis-main/cnu_ai_senti_analysis-main/collector/collect_naver_movie_review.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+#######################
+# 1. 영화 제목 수집
+#######################
+
+# movie_code: 네이버 영화 코드(6자리 숫자)
+
+# 제목 수집
+# 함수생성
+# - 1.생성, 2.호출
+# - 함수는 생성하면 아무 동장 x
+# - 반드시 생성 후 호출을 통해서 사용!
+def movie_title_crawler(movie_code):
+    url = f'https://movie.naver.com/movie/bi/mi/point.naver?code={movie_code}'
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, 'html.parser')
+    title = doc.select('h3.h_movie > a')[0].get_text()
+    return title
+
+# 리뷰 수집(리뷰, 평점, 작성자, 작성일자) + 제목
+def movie_review_crawler(movie_code):
+    title = movie_title_crawler(movie_code)   # 제목 수집
+    print(f'제목: {title}')
+    # 리뷰를 수집하는 코드 작성
+    url = 'https://movie.naver.com/movie/bi/mi/pointWriteFormList.naver?code=191657&type=after&isActualPointWriteExecute=false&isMileageSubscriptionAlready=false&isMileageSubscriptionReject=false&page=6'
+
+    # set {제목, 리뷰, 평점, 작성자, 작성일자}
---
+++ cnu_ai_senti_analysis-main/cnu_ai_senti_analysis-main/main.py
@@ -0,0 +1,8 @@
+from collector.collect_naver_movie_review import movie_review_crawler
+
+
+# 프로그램 실행!
+# - 필요한 기능들을 호출해서 사용!
+movie_code = '191657' # 네이버 영화 코드
+movie_review_crawler(movie_code)
+
